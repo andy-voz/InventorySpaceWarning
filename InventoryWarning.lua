0969bee29f4df78667e23fa36101114a2f27f821
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2023-07-09 15:12:16 -0400

    Saving indicator's position.

--- InventoryWarning.lua
+++ InventoryWarning.lua
@@ -5,7 +5,12 @@ InventoryWarning.name = "InventoryWarning"
 
 local logger = LibDebugLogger(InventoryWarning.name)
 
-local function _checkFreeSlots()
+function InventoryWarning.OnIndicatorMoveStop()
+    InventoryWarning.savedVariables.left = InventoryWarningIndicator:GetLeft()
+    InventoryWarning.savedVariables.top = InventoryWarningIndicator:GetTop()
+end
+
+function InventoryWarning.CheckFreeSlots()
     InventoryWarning.freeSlots = GetNumBagFreeSlots(BAG_BACKPACK)
 
     logger:Debug("Free backpack slots count: %d", InventoryWarning.freeSlots)
@@ -15,23 +20,32 @@ end
 
 local function _onInventoryChanged(_eventCode, _bagId, slotIndex, _isNewItem, _itemSoundCategory, updateReason, _stackCountChange)
     logger:Debug("Inventory changed. slotIndex: %s, updateReason: %s", slotIndex, updateReason)
-    _checkFreeSlots()
+    InventoryWarning.CheckFreeSlots()
 end
 
-function InventoryWarning.Initialize()
-    _checkFreeSlots()
+local function _restorePosition()
+    local left = InventoryWarning.savedVariables.left
+    local top = InventoryWarning.savedVariables.top
 
+    InventoryWarningIndicator:ClearAnchors()
+    InventoryWarningIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
+end
+
+local function _initialize()
     local fragment = ZO_HUDFadeSceneFragment:New(InventoryWarningIndicator, nil, 0)
     HUD_SCENE:AddFragment(fragment)
     HUD_UI_SCENE:AddFragment(fragment)
 
     EVENT_MANAGER:RegisterForEvent(InventoryWarning.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, _onInventoryChanged)
     EVENT_MANAGER:AddFilterForEvent(InventoryWarning.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, REGISTER_FILTER_BAG_ID, BAG_BACKPACK)
+
+    InventoryWarning.savedVariables = ZO_SavedVars:NewCharacterIdSettings("InventoryWarningSavedVariables", 1, nil, {})
+    _restorePosition()
 end
 
 function InventoryWarning.OnAddOnLoaded(event, addonName)
     if addonName == InventoryWarning.name then
-        InventoryWarning.Initialize()
+        _initialize()
         EVENT_MANAGER:UnregisterForEvent(InventoryWarning.name, EVENT_ADD_ON_LOADED)
     end
 end
