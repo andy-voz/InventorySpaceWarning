aa7fc2d2338feba96578abbcb8211d59cab2bde3
Andrey Voznyuk <andrey.voznyuk.dev@gmail.com>
2023-08-13 14:10:06 -0400

    Fixed files structure.

--- pack.sh
+++ pack.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 rm -rf .pack
-mkdir -p .pack/InventorySpaceWarning/res
-cp InventorySpaceWarning.* .pack/InventorySpaceWarning/
-cp res/*.dds .pack/InventorySpaceWarning/res/
+
+mkdir -p .pack/InventorySpaceWarning
+
+rsync -a InventorySpaceWarning.txt .pack/InventorySpaceWarning/
+rsync -a src .pack/InventorySpaceWarning/
+rsync -a ui .pack/InventorySpaceWarning/
+rsync -a res/*.dds .pack/InventorySpaceWarning/res/
+
 cd .pack
 zip -r InventorySpaceWarning.zip InventorySpaceWarning
 cd -
